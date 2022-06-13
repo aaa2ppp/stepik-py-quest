12c13b95a4d55c8fd925299107625bbbc3266c07
aaa2ppp <aleksandr@panfilov.pp.ru>
2022-06-14 02:47:54 +0300

    count_of fix2

--- game/helpers.py
+++ game/helpers.py
@@ -6,7 +6,9 @@ def count_of(count: int, *word_forms: Iterable[str]):
         return f"{count} {word_forms[2]}"
     else:
         _count = count % 10
-        if _count == 1:
+        if _count == 0;
+            return f"{count} {word_forms[2]}"
+        elif _count == 1:
             return f"{count} {word_forms[0]}"
         elif _count < 5:
             return f"{count} {word_forms[1]}"
