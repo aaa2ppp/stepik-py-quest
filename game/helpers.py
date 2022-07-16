5588629f41334d9cc6367ab3201e8bc9cd538811
aaa2ppp <alexander@panfilov.pp.ru>
2022-07-16 05:50:14 +0300

    Update helpers.py
    fixed typo, semicolon to colon

--- game/helpers.py
+++ game/helpers.py
@@ -6,7 +6,7 @@ def count_of(count: int, *word_forms: Iterable[str]):
         return f"{count} {word_forms[2]}"
     else:
         _count = count % 10
-        if _count == 0;
+        if _count == 0:
             return f"{count} {word_forms[2]}"
         elif _count == 1:
             return f"{count} {word_forms[0]}"
